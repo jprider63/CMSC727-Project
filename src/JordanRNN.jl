eda601a4fe1139d2c0493cf3281c826d8e0374be
Kerry Cheng <kerry.cheng.2013@gmail.com>
2013-04-17 12:30:54 -0400

    Started working on JordanRNN
    JordanNetwork type defined
    JordanNetwork constructor defined
    train and evaluate function to come soon

--- src/JordanRNN.jl
+++ src/JordanRNN.jl
@@ -1,3 +1,22 @@
 module JordanRNN
+    type JordanNetwork
+        weightsHI::Matrix(Float)
+        weightsHC::Matrix(Float)
+        weightsOH::Matrix(Float)
 
+        function JordanNetwork(n_inputs::Int, n_outputs::Int, n_hidden::Int, )
+            weightsHI = rand(n_hidden, n_input) - .5
+            weightsHC = rand(n_hidden, n_output) - .5
+            weightsOH = rand(n_output, n_hidden) - .5
+            new(weightsHI, weightsHC, weightsOH)
+        end
+    end
+
+    function JordanTrain(network::JordanNetwork, )
+
+    end
+
+    function JordanEvaluate(network::JordanNetwork, input::Vector{Float})
+
+    end
 end
