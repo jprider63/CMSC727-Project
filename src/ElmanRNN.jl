e34b26f1cdf861ef50b51d627ccb1453c8d27e6d
jp.rider63 <dev@jamesparker.me>
2013-04-17 12:13:23 -0400

    Started work on Elman RNN.

--- src/ElmanRNN.jl
+++ src/ElmanRNN.jl
@@ -1,3 +1,33 @@
 module ElmanRNN
 
+type ElmanNetwork
+	weightsHI::Matrix{Float} # Weights from input to hidden layer.
+	weightsHC::Matrix{Float} # Weights from context to hidden layer.
+	weightsOH::Matrix{Float} # Weights from hidden to output layer.
+	# functions (w/ constants like eta, mu included in closures) for error function, context, etc?
+
+
+	function ElmanNetwork( inputSize::Int, hiddenSize::Int, outputSize::Int)
+		wHI = rand( hiddenSize, inputSize)
+		wHC = rand( hiddenSize, outputSize)
+		wOH = rand( outputSize, hiddenSize)
+
+		new( wHI, wHC, wOH)
+	end
+end
+
+function ElmanTrain( network::ElmanNetwork, inputs::Matrix{Float}, target::Vector{Float})
+	# Check dimensions of inputs.
+
+	contextLayer = 
+
+
+	# return error, number of epochs?
+end
+
+function ElmanEvaluate( network::ElmanNetwork, input::Vector{Float})
+	# Check dimensions of inputs.
+
+end
+
 end
