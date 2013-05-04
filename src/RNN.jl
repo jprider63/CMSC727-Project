b0a3e4b7a3c5d18ca9e81db0b07a20be319e9817
jp.rider63 <dev@jamesparker.me>
2013-05-04 17:26:08 -0400

    Fixed the name of the L2NormError.

--- src/RNN.jl
+++ src/RNN.jl
@@ -1,41 +1,45 @@
 module RNN
 
-export Float
-export TimeSeriesSample, TimeSeriesSamples, ActivationRule, defaultLearningRate, defaultActivationRule, logisticActivationRule
+export TimeSeriesSample, TimeSeriesSamples, ActivationRule, defaultLearningRate, defaultActivationRule, logisticActivationRule, ErrorFunction
 
 immutable TimeSeriesSample
-	sample::Matrix{Float} # |Input or Output| x time Matrix
+	sample::Matrix{Float64} # |Input or Output| x time Matrix
 end
 
 immutable TimeSeriesSamples
 	samples::Vector{TimeSeriesSample} # Vector of samples.
-		sizeSample::Int # The |Input or Output| of each sample.
+	sizeSample::Int # The |Input or Output| of each sample.
 
-		function TimeSeriesSamples( samples::Vector{TimeSeriesSample})
-			if length( samples) == 0
-				return new( samples, 0)
-			end
-
-			# Check that the sizes of each sample's Input or Output are equal.
-			sizeSample = size( samples[1], 1)
-			for sample in samples
-				if size( sample, 1) != sizeSample
-					error( "The sizes of each sample's Input or Output must be equal.")
-				end
-			end
-
-			new( samples, sizeSample)
+	function TimeSeriesSamples( samples::Vector{TimeSeriesSample})
+		if length( samples) == 0
+			return new( samples, 0)
 		end
+
+		# Check that the sizes of each sample's Input or Output are equal.
+		sizeSample = size( samples[1], 1)
+		for sample in samples
+			if size( sample, 1) != sizeSample
+				error( "The sizes of each sample's Input or Output must be equal.")
+			end
+		end
+
+		new( samples, sizeSample)
+	end
 end
 
+# Define the type of error function. Both vectors must have equal lengths.
+# Vector{Float64} * Vector{Float64} -> Float64
+# TODO: Once Julia supports it, enforce this type on the function.
+typealias ErrorFunction Function
+
 immutable ActivationRule
-	activationFunction::Function
-	activationDerivative::Function
+	activationFunction::Function # TODO: Once Julia supports it, enforce the type on the function.
+	activationDerivative::Function # TODO: Once Julia supports it, enforce the type on the function.
 end
 
 const defaultLearningRate = .05
 const defaultMaxEpochs = typemax(Uint)
-const defaultErrorThreshold = .0005
+const defaultErrorThreshold = .5
 
 function defaultActivationRule()
 	logisticActivationRule(1)
@@ -48,10 +52,11 @@ function logisticActivationRule(s)
 end
 
 function defaultErrorFunction()
-	meanSquaredError
+	L2NormError
 end
 
-function meanSquaredError( output::Vector{Float}, target::Vector{Float})
+# Compute the L2 norm error.
+function L2NormError( output::Vector{Float64}, target::Vector{Float64})
 	# Check that the two vectors of are equal lengths.
 	if length( output) != length( target)
 		error( "The length of the two vectors must be equal.")
