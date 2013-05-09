feee48dc95aed99e2e4cb96f775123631cc23500
jp.rider63 <dev@jamesparker.me>
2013-05-09 00:19:30 -0400

    updated exchanger script

--- test/exchangerTest.jl
+++ test/exchangerTest.jl
@@ -4,6 +4,8 @@ require("helper.jl")
 using RNN
 using ElmanRNN
 
+srand(63)
+
 data = importData("exchanger")
 dataInput = data[:,1]
 dataOutput = data[:,2]
@@ -15,6 +17,13 @@ samplesInput = TimeSeriesSamples(vectorInput)
 samplesOutput = TimeSeriesSamples(vectorOutput)
 
 net = ElmanNetwork(1, 10, 1)
+net.mu = .1
+net.eta = .01
+net.errorThreshold = .001
 ElmanTrain!(net, samplesInput, samplesOutput)
 
-print(net)
+#print(net)
+
+target = ElmanEvaluate( net, sampleInput)
+
+print( target - dataOutput')
